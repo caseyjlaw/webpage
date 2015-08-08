ec621920bd81ebdab14f6ec5ca8edff4fd6e2dad
Casey Law <caseyjlaw@gmail.com>
2015-08-08 14:19:25 -0700

    published on github. not too embarrassing

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,19 +35,22 @@ MARKUP = ('md', 'ipynb')
 
 LANDING_PAGE_ABOUT = {
 #    'title': 'Casey J Law',
-    'details': 'An Astronomer in the UC Berkeley Radio Astronomy Lab. I am interested in fast radio transients, data-intensive science, Python, and Civic Hacking.'
+    'details': 'An Astronomer in the UC Berkeley Radio Astronomy Lab. I am interested in fast radio transients, data-intensive science, Python, and Civic Hacking.' # <img width=200 align="right" src="images/casey_vla.jpg">
     }
 PROJECTS = [
     {'name': 'Fast Imaging with Radio Interferometers',
-     'url': '',
+     'url': 'http://labs.adsabs.harvard.edu/adsabs/abs/2015ApJ...807...16L',
      'description': 'Using radio interferometers as high-speed cameras to capture millisecond impulses from rare transients like fast radio bursts, pulsars, and stars.'
      },
     {'name': 'Real-time data analysis',
-     'url': '',
+     'url': 'https://www.authorea.com/users/4091/articles/4912/_show_article',
      'description': 'Developing the Very Large Array as a real-time, commensal transient search platform.'
+     },
+    {'name': 'VLA Sky Survey',
+     'url': 'https://science.nrao.edu/science/surveys/vlass',
+     'description': 'I am co-chair of the technical working group for an ambitious new sky survey with the world\'s most sensitive radio interferometer.'
      }
     ]
-        
 
 MD_EXTENSIONS = ['codehilite(css_class=highlight)', 'extra', 'headerid', 'toc']
 DIRECT_TEMPLATES = (('index', 'tags', 'categories','archives', 'search', '404'))
@@ -62,8 +65,9 @@ EXTRA_PATH_METADATA = {
     'extra/favicon.ico': {'path': 'favicon.ico'}
     }
 
-HOME_EXCLUDE = True
+HOME_EXCLUDE = False
 DEFAULT_PAGINATION = False
 CACHE_CONTENT = False
+LOAD_CONTENT_CACHE = False
 DISPLAY_CATEGORIES_ON_MENU = False
 DISPLAY_TAGS_ON_MENU = False
